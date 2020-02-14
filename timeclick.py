da84a7e714dfcb08814d019a9ef51dd3fadf26bb
Jay Kshirsagar <kshirsagarjay522000@gmail.com>
2020-02-15 02:18:53 +0530

    raspberry to firebase edited but little pending

--- timeclick.py
+++ timeclick.py
@@ -1,10 +1,20 @@
 import cv2
 import time
 import datetime
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use the application default credentials
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+  'projectId': autoattendance-91502,
+})
+
+db = firestore.client()
 
 
 def photo_click():
-
     count = 0
     cap = cv2.VideoCapture(0)
     _, frame = cap.read()
@@ -15,6 +25,15 @@ def photo_click():
     file = time.strftime("%H:%M:%S", t).split(":")
     a = ''
     file_name = file_name + a.join(file)
+    img_name = file_name
+    img = cap.read()
+    #Section written by jay for save image to database
+    data = {
+        "Image": img
+    }
+    doc_ref = db.collection('test_images').document(img_name)
+    doc_ref.set(data)
+    #section end
     print(file_name)
     cv2.imwrite(file_name + ".jpg", frame)
     cap.release()
